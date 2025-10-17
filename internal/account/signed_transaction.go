44cec35aa17749502092cca6b46fd174c3b52b6b
3lv <cristian.vlad.vladutu@gmail.com>
2025-10-17 14:22:27 +0200

    Created signed transaction struct and helpers

---
+++ internal/account/signed_transaction.go
@@ -0,0 +1,82 @@
+package account
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"ledger/internal/signature"
+	"math/big"
+)
+
+type SignedTransaction struct {
+	ID        string
+	From      string
+	To        string
+	Amount    int
+	Signature string
+}
+
+func NewSignedTransaction(id, from, to string, amount int, sk *signature.SecretKey) *SignedTransaction {
+	tx := &SignedTransaction{
+		ID:     id,
+		From:   from,
+		To:     to,
+		Amount: amount,
+	}
+	ser := Serialize(tx)
+	sig := signature.Sign(ser, sk).String()
+	tx.Signature = sig
+
+	return tx
+}
+
+func AddStr(b *bytes.Buffer, s string) error {
+	err := binary.Write(b, binary.BigEndian, uint32(len(s)))
+	if err != nil {
+		return err
+	}
+	b.WriteString(s)
+	return nil
+}
+
+func AddInt(b *bytes.Buffer, val int) error {
+	err := binary.Write(b, binary.BigEndian, int64(val))
+	return err
+}
+
+// Does not include the Signature, meant to be used for signing
+// [ID][From][To][Amount]
+func Serialize(tx *SignedTransaction) []byte {
+	var b bytes.Buffer
+
+	AddStr(&b, tx.ID)
+	AddStr(&b, tx.From)
+	AddStr(&b, tx.To)
+	AddInt(&b, tx.Amount)
+
+	return b.Bytes()
+}
+
+func (l *Ledger) ExecuteSignedTransaction(tx *SignedTransaction) error {
+	// Check signature
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	ser := Serialize(tx)
+	sig := new(big.Int)
+	sig.SetString(tx.Signature, 10)
+	pk, err := signature.Decode(tx.From)
+	if err != nil {
+		return err
+	}
+
+	if !signature.Verify(ser, sig, pk) {
+		// invalid signature
+		return fmt.Errorf("invalid signature for transaction %s", tx.ID)
+	}
+
+	l.Accounts[tx.From] -= tx.Amount
+	l.Accounts[tx.To] += tx.Amount
+
+	return nil
+}
---
+++ internal/account/signed_transaction_test.go
@@ -0,0 +1,34 @@
+package account
+
+import (
+	"ledger/internal/signature"
+	"math/big"
+	"testing"
+)
+
+func TestSignedTransactionSignature(t *testing.T) {
+	kp1 := signature.DefaultKeyGen()
+	kp2 := signature.DefaultKeyGen()
+	tx := NewSignedTransaction("tx1", kp1.Pk.Encode(), kp2.Pk.Encode(), 100, kp1.Sk)
+	ser := Serialize(tx)
+	sig := new(big.Int)
+	sig.SetString(tx.Signature, 10)
+	if !signature.Verify(ser, sig, kp1.Pk) {
+		t.Errorf("Failed to verify signed transaction signature")
+	}
+}
+
+func TestSignedTransactionExecution(t *testing.T) {
+	kp1 := signature.DefaultKeyGen()
+	kp2 := signature.DefaultKeyGen()
+	ledger := MakeLedger()
+	ledger.Accounts[kp1.Pk.Encode()] = 100
+	ledger.Accounts[kp2.Pk.Encode()] = 50
+	tx := NewSignedTransaction("tx1", kp1.Pk.Encode(), kp2.Pk.Encode(), 100, kp1.Sk)
+	if err := ledger.ExecuteSignedTransaction(tx); err != nil {
+		t.Errorf("Failed to execute signed transaction: %v", err)
+	}
+	if ledger.Accounts[kp1.Pk.Encode()] != 0 || ledger.Accounts[kp2.Pk.Encode()] != 150 {
+		t.Errorf("Ledger accounts not updated correctly after transaction")
+	}
+}
