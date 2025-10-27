e6dc77fd5c59c8edcbc55b74c66d0975336a235c
3lv <cristian.vlad.vladutu@gmail.com>
2025-10-27 12:39:27 +0100

    Test rejecting invalid sign
    Neccesary for Exercise 6.16 requirements

--- internal/account/signed_transaction_test.go
+++ internal/account/signed_transaction_test.go
@@ -32,3 +32,16 @@ func TestSignedTransactionExecution(t *testing.T) {
 		t.Errorf("Ledger accounts not updated correctly after transaction")
 	}
 }
+
+func TestRejectInvalidTransaction(t *testing.T) {
+	kp1 := signature.DefaultKeyGen()
+	kp2 := signature.DefaultKeyGen()
+	wrongKp := signature.DefaultKeyGen()
+	ledger := MakeLedger()
+	ledger.Accounts[kp1.Pk.Encode()] = 50
+	ledger.Accounts[kp2.Pk.Encode()] = 50
+	tx := NewSignedTransaction(kp1.Pk.Encode(), kp2.Pk.Encode(), 30, wrongKp.Sk)
+	if err := ledger.ExecuteSignedTransaction(tx); err == nil {
+		t.Errorf("Expected error when executing transaction with invalid signature")
+	}
+}
